50a6279ca6e35e7a062ea074d80cd0d1782eb3b4
mintxdp <dheerajprakashofficial1232@gmail.com>
2024-05-12 17:36:55 +0530

    new files added

---
+++ main.js
@@ -0,0 +1,7 @@
+let openbtn = document.querySelector('.arrow_h')
+let project = document.querySelector('.projectbar')
+
+openbtn.addEventListener('click',function (){
+project.style.border='solid black 1px'
+console.log('clicked')
+})
